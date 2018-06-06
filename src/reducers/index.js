40e46fe8ce7bf3f95acf6cfa20cd59db466e5ef6
Ashley <AshleyReneeLowry@gmail.com>
2018-06-06 13:37:01 -0500

    add some form fields

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+
 import auth from './auth';
 /*
 Redux State auth: {
 authenticated: string
 error: string }
 */
-export default combineReducers({ auth });
+export default combineReducers({
+  auth,
+  form: formReducer
+});
